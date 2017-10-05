a46c0644c2c9fd1c60b29a2b6dc70a8d1eb93392
Mike Posthumus <mposthumus@gmail.com>
2017-10-04 21:59:34 -0400

    Added failure message.

--- js/main.js
+++ js/main.js
@@ -10,10 +10,13 @@ var faceValue = 0;
 // Form initialization
 //
 $("#roll").click(function() {
+  faceValue = 0;
     $("#submissionForm").show();
+    $(".correct").remove();
+    var input = $("#val").val('');
 
 var diceArea$ = $("#diceArea");
-
+diceArea$.find(".dice").remove();
     for (var i=1; i <= DICE_COUNT; i++) {
       var dice$ = $("<div class='dice'>");
       var val = Math.floor(Math.random() * 6) +1;
@@ -24,11 +27,21 @@ var diceArea$ = $("#diceArea");
 });
 
 $("#submit").click(function() {
+
 var input = $("#val").val();
 input = parseInt(input)
 if (input === faceValue) {
     $("#submissionForm").append($("<div class='correct'>").text("Nice Job Einstein!"));
 
 
+} else {
+  $("#submissionForm").append($("<div class='fail'>").text("Keep Trying"));
 }
 });
+
+
+// add timer performance.now();
+// add remove success status
+// add failure status (one static status box)
+// on roll set status to ''
+//
