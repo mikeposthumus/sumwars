d08010eca214db18ff1a65118a258bfe8c2b03a0
Mike Posthumus <mposthumus@gmail.com>
2017-10-06 00:02:51 -0400

    Added results times. Add functions to give input focus. Now gamestate clears on submit. Removed submit on correct answer. Added Headings

--- js/main.js
+++ js/main.js
@@ -6,14 +6,31 @@ var DICE_COUNT = 4;
 //
 // Global
 var faceValue = 0;
+var timerStart = 0;
+var timerCheck = 0;
 //
 // Form initialization
 //
 $("#roll").click(function() {
   faceValue = 0;
+  timerStart = performance.now();
     $("#submissionForm").show();
     $(".correct,.fail").remove();
-    var input = $("#val").val('');
+    var input = $("#val").val('').focus();
+
+    $("#val").keyup(function () {
+           if (this.value.length == this.maxLength) {
+             $(this).next().focus();
+           }
+     });
+
+    var timer$ = $("#timer");
+    var time$ = $("<div class='time'>")
+    timer$.find(".time").remove();
+    $("#timer").show().append(time$.text("Timer Fx Here"));
+    $("#submit").show();
+
+
 
 var diceArea$ = $("#diceArea");
 diceArea$.find(".dice").remove();
@@ -27,26 +44,29 @@ diceArea$.find(".dice").remove();
 });
 
 $("#submit").click(function() {
-
+var timerCheck = Math.floor((performance.now() - timerStart)) / 1000;
 var input = $("#val").val();
 var statusArea$ = $("#statusArea");
-
 input = parseInt(input)
 
 if (input === faceValue) {
     $(".correct,.fail").remove();
-    $("#statusArea").append($("<div class='correct'>").text("Nice Job Einstein! Play again."));
+    var timerEnd = timerCheck
+    $("#statusArea").append($("<div class='correct'>").text("Nice work. You got it right in " + (timerEnd) + " seconds. Hit Enter to Sum again!"));
+    $("#submit").hide();
+    $("#roll").focus();
 
 
 } else {
   $(".correct,.fail").remove();
-  $("#statusArea").append($("<div class='fail'>").text("Keep Trying"));
+  $("#statusArea").append($("<div class='fail'>").text("It took you " + (timerCheck) + " seconds to come up with a wrong answer... try again."));
+  input = $("#val").val('').focus();
 }
 });
 
 
-// add timer performance.now();
+// add timer performance.now(); x
 // add remove success status X
 // add failure status (one static status box) X
-// on roll set status to ''
+// on roll set status to '' x
 //
